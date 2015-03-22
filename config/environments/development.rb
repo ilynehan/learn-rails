59e4dc8671ee1506d1aa92602557766afa958d16
ILYNE HAN <ilynehan@gmail.com>
2015-03-22 03:10:21 -0700

    fixed typos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ config.action_mailer.smtp_settings = {
 # ActionMailer Config
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_deliver_errors = true
+config.action_mailer.raise_delivery_errors = true
 
 # Send email in development mode?
 config.action_mailer.perform_deliveries = true
